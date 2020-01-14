ae2956067fe30a16baa9c48cc60c804074f82fa1
Shantur Rathore <shantur.rathore@gmail.com>
2020-01-14 11:07:41 +0000

    Initial tasui-dev.shantur.com

---
+++ precache-manifest.8da610e28726104c7f554917a9a36a67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415362e6da10e042eacc86964ed2da88",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b645b5d2efdb4418423",
+    "url": "./static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "6cb0401ee94586c4398c",
+    "url": "./static/js/2.10fdf520.chunk.js"
+  },
+  {
+    "revision": "2b645b5d2efdb4418423",
+    "url": "./static/js/main.21ac6d8d.chunk.js"
+  },
+  {
+    "revision": "8e535f8e9d7ad097535b",
+    "url": "./static/js/runtime-main.195c3e23.js"
+  }
+]);
